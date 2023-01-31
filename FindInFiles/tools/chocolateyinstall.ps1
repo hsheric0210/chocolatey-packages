62af7b12ed81c44280100ca7ca576e9a658ce197
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-02-01 05:00:49 +0900

    excluded template from au
    [-] Excluded template package from auto-update
    [/] Fix for mismatched nuspec name in template package (which caused au failure)

--- FindInFiles/tools/chocolateyinstall.ps1
+++ FindInFiles/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 # Will be automatically filled
 $packageArgs = @{
   packageName   = 'FindInFiles'
-  fileType      = 'EXE'
+  fileType      = 'exe'
   url           = 'https://github.com/toolscode/findinfiles/releases/download/v3.6.26/findinfiles_win_x64_b416.exe'
-  softwareName  = 'FindInFiles*'
-  checksum      = '94F6F57C60B6D92F03BAA40BD75FE0092F2E9B04222243CB763D796F7702C27D'
+  softwareName  = 'FindInFiles *'
+  checksum      = '94f6f57c60b6d92f03baa40bd75fe0092f2e9b04222243cb763d796f7702c27d'
   checksumType  = 'sha256'
   silentArgs   = '/S'
   validExitCodes= @(0)
