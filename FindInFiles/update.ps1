a20fcdf67217ed2d8b28050dccbff6700d70fccb
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-02-01 05:29:19 +0900

    add FindInFiles tags

--- FindInFiles/update.ps1
+++ FindInFiles/update.ps1
@@ -8,6 +8,7 @@ function global:au_GetLatest {
     Get-GitHubLatest `
         -Repository 'toolscode/findinfiles' `
         -FileType 'exe' `
+        -IsUri64 { return $true } `
         -Latest @{
             SoftwareName   = 'FindInFiles *'
             SilentArgs     = '/S'
@@ -21,10 +22,10 @@ function global:au_SearchReplace {
             "^(\s*packageName\s*=\s*)'.*'$"       = "`$1'$($Latest.PackageName)'"
             "^(\s*softwareName\s*=\s*)'.*'$"      = "`$1'$($Latest.SoftwareName)'"
             "^(\s*fileType\s*=\s*)'.*'$"          = "`$1'$($Latest.FileType)'"
-            "^(\s*url\s*=\s*)'.*'$"               = "`$1'$($Latest.Url32)'"
+            "^(\s*url\s*=\s*)'.*'$"               = "`$1'$($Latest.Url64)'"
             "^(\s*silentArgs\s*=\s*)'.*'$"        = "`$1'$($Latest.SilentArgs)'"
-            "^(\s*checksum\s*=\s*)'.*'$"          = "`$1'$($Latest.Checksum32)'"
-            "^(\s*checksumType\s*=\s*)'.*'$"      = "`$1'$($Latest.ChecksumType32)'"
+            "^(\s*checksum\s*=\s*)'.*'$"          = "`$1'$($Latest.Checksum64)'"
+            "^(\s*checksumType\s*=\s*)'.*'$"      = "`$1'$($Latest.ChecksumType64)'"
             "^(\s*validExitCodes\s*=\s*)@\(.*\)$" = "`$1@($($Latest.ValidExitCodes))"
         }
     }
